65e76853a1c72a8b2964ca51c71e18668b52897a
Paul Wighton <paul.wighton@gmail.com>
2023-10-06 12:51:33 -0400

    [ENH] generalizing tsv read

--- integrate.py
+++ integrate.py
@@ -12,6 +12,10 @@ def parse_args():
     parser.add_argument('-a', '--aif', help='Arterial input function data, currently expects output of bloodstream', required=True)
     parser.add_argument('-b', '--bids', help='BIDS sidecar file to PET data; expecting to find FrameTimesStart and FrameDuration', required=True)
     parser.add_argument('-o', '--out', help='Filename to write test results to, one per line', default='aif.bloodstream.dat')
+    parser.add_argument('-tc', '--time-col', help='The name of the time column in the tsv (default="time")',
+                        default='time')
+    parser.add_argument('-ac', '--aif-col', help='Then name of the aif column in the tsv (deafult="AIF")',
+                        default='AIF')
     return parser.parse_args()
 
 def main(cmd_args):
@@ -24,8 +28,8 @@ def main(cmd_args):
 
     # Read bloodstream output
     blood_data_df = pandas.read_csv(args.aif, delimiter='\t')
-    blood_aif = blood_data_df[["AIF"]].to_numpy().squeeze()
-    blood_time = blood_data_df[["time"]].to_numpy().squeeze()
+    blood_aif = blood_data_df[[args.aif_col]].to_numpy().squeeze()
+    blood_time = blood_data_df[[args.time_col]].to_numpy().squeeze()
 
     # Calc Frame start and end times
     frame_start_times = np.asarray(bids_sidecar["FrameTimesStart"], dtype=np.float32).squeeze()
@@ -45,4 +49,3 @@ def main(cmd_args):
 	
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
-
