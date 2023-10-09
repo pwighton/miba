9dd31c0f657af97b76f21ce7087a42ccbf2d5f64
Paul Wighton <paul.wighton@gmail.com>
2023-10-09 12:54:22 -0400

    [FIX] to match cox1-proc

--- cox1-preproc.bash
+++ cox1-preproc.bash
@@ -67,8 +67,8 @@ do
     echo "cp ${IMG_REF_DIR}/${PET_IMG_FILE} ${PETSURFER_DIR}/pet.nii.gz"
     cp ${IMG_REF_DIR}/${PET_IMG_FILE} ${PETSURFER_DIR}/pet.nii.gz
 
-    echo "cp ${MOCO_REF_DIR}/${PET_MOCO_FILE} ${PETSURFER_DIR}/pet.mc.nii.gz"
-    cp ${MOCO_REF_DIR}/${PET_MOCO_FILE} ${PETSURFER_DIR}/pet.mc.nii.gz
+    echo "cp ${MOCO_REF_DIR}/${PET_MOCO_FILE} ${PETSURFER_DIR}/pet.mn.nii.gz"
+    cp ${MOCO_REF_DIR}/${PET_MOCO_FILE} ${PETSURFER_DIR}/pet.mn.nii.gz
 
     echo "${CALC_FRAMEWISE_AIF_PY} -a ${BLOOD_REF_DIR}/${BLOODSTREAM_FILE} -b ${IMG_REF_DIR}/${PET_JSON_FILE} -o ${PETSURFER_DIR}/aif.bloodstream.dat"
     ${CALC_FRAMEWISE_AIF_PY} -a ${BLOOD_REF_DIR}/${BLOODSTREAM_FILE} -b ${IMG_REF_DIR}/${PET_JSON_FILE} -o ${PETSURFER_DIR}/aif.bloodstream.dat
