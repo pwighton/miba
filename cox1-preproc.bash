3d7e8951a1d2d7e3baa466faf613a71ef27a8f16
Paul Wighton <paul.wighton@gmail.com>
2023-10-09 12:22:22 -0400

    [FIX] updating cox1-preproc for moco pet data

--- cox1-preproc.bash
+++ cox1-preproc.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# $SUBJECTS_DIR should be set before run
+# 
 
 # Location of mapping file, each line has
 #  - sub-name
@@ -9,20 +9,27 @@
 #  - bids pet img sidecar (json)
 #    - assumes an corresponding .nii.gz file also exists
 #  - bloodstram file  
-MAP_FILE="/autofs/vast/gerenuk/pwighton/pet/miba/cox1-preproc-mapping.txt"
+
+MAP_FILE="cox1-preproc-mapping.txt"
+
+SUBJECTS_DIR="/autofs/vast/gerenuk/pwighton/pet/fs-subs"
 
 # Top level directory of pet imaging data
 IMG_REF_DIR="/autofs/vast/gerenuk/pwighton/pet/ds004230-plus-cox1blocked"
+
+# Top level directory of motion corrected pet imaging data
+MOCO_REF_DIR="/autofs/vast/gerenuk/pwighton/pet/ds004230-plus-cox1blocked--petprep-hmc--384176a6cc6b"
 
 # Top level directory of bloodstream data
 BLOOD_REF_DIR="/autofs/vast/gerenuk/pwighton/pet/ds004230-plus-cox1blocked--bloodstream2023-08-28_id-tfK8--martin"
 
 # Location of integrate.py
-CALC_FRAMEWISE_AIF_PY="/autofs/vast/gerenuk/pwighton/pet/miba/calc_framewise_aif.py"
+CALC_FRAMEWISE_AIF_PY="calc_framewise_aif.py"
 
 echo "SUBJECTS_DIR:         "$SUBJECTS_DIR
 echo "MAP_FILE:             "$MAP_FILE
 echo "IMG_REF_DIR:          "$IMG_REF_DIR
+echo "MOCO_REF_DIR:         "$MOCO_REF_DIR
 echo "BLOOD_REF_DIR:        "$BLOOD_REF_DIR
 
 # Make the file tsec.txt
@@ -37,14 +44,16 @@ do
     SUB_NAME=$(cat $MAP_FILE| sed -n "${LINE_NUM}p" | awk '{print $1}')
     PET_SUBDIR=$(cat $MAP_FILE | sed -n "${LINE_NUM}p" | awk '{print $2}')
     PET_JSON_FILE=$(cat $MAP_FILE | sed -n "${LINE_NUM}p" | awk '{print $3}')
-    BLOODSTREAM_FILE=$(cat $MAP_FILE | sed -n "${LINE_NUM}p" | awk '{print $4}')
+    PET_MOCO_FILE=$(cat $MAP_FILE | sed -n "${LINE_NUM}p" | awk '{print $4}')
+    BLOODSTREAM_FILE=$(cat $MAP_FILE | sed -n "${LINE_NUM}p" | awk '{print $5}')
     
-    echo "SUB_NAME:         "$SUB_NAME
-    echo "PET_SUBDIR:       "$PET_SUBDIR
-    echo "BLOODSTREAM_FILE: "$BLOODSTREAM_FILE
-    echo "PET_JSON_FILE:    "$PET_JSON_FILE
+    echo "SUB_NAME:          "$SUB_NAME
+    echo "PET_SUBDIR:        "$PET_SUBDIR
+    echo "BLOODSTREAM_FILE:  "$BLOODSTREAM_FILE
+    echo "PET_JSON_FILE:     "$PET_JSON_FILE
     PET_IMG_FILE=$(echo "${PET_JSON_FILE%%.*}")".nii.gz"
-    echo "PET_IMG_FILE:     "$PET_IMG_FILE
+    echo "PET_IMG_FILE:      "$PET_IMG_FILE
+    echo "PET_MOCO_FILE:     "$PET_MOCO_FILE
     
     PETSURFER_DIR=$SUBJECTS_DIR/$SUB_NAME/$PET_SUBDIR
     echo "PETSURFER_DIR:    "$PETSURFER_DIR 
@@ -58,9 +67,9 @@ do
     echo "cp ${IMG_REF_DIR}/${PET_IMG_FILE} ${PETSURFER_DIR}/pet.nii.gz"
     cp ${IMG_REF_DIR}/${PET_IMG_FILE} ${PETSURFER_DIR}/pet.nii.gz
 
-    echo "mc-afni2 --i ${PETSURFER_DIR}/pet.nii.gz --o ${PETSURFER_DIR}/pet.mc.nii.gz --mcdat ${PETSURFER_DIR}/mc-afni2--motion-parameters.dat"
-    mc-afni2 --i ${PETSURFER_DIR}/pet.nii.gz --o ${PETSURFER_DIR}/pet.mc.nii.gz --mcdat ${PETSURFER_DIR}/mc-afni2--motion-parameters.dat
-    
+    echo "cp ${MOCO_REF_DIR}/${PET_MOCO_FILE} ${PETSURFER_DIR}/pet.mc.nii.gz"
+    cp ${MOCO_REF_DIR}/${PET_MOCO_FILE} ${PETSURFER_DIR}/pet.mc.nii.gz
+
     echo "${CALC_FRAMEWISE_AIF_PY} -a ${BLOOD_REF_DIR}/${BLOODSTREAM_FILE} -b ${IMG_REF_DIR}/${PET_JSON_FILE} -o ${PETSURFER_DIR}/aif.bloodstream.dat"
     ${CALC_FRAMEWISE_AIF_PY} -a ${BLOOD_REF_DIR}/${BLOODSTREAM_FILE} -b ${IMG_REF_DIR}/${PET_JSON_FILE} -o ${PETSURFER_DIR}/aif.bloodstream.dat
 done
