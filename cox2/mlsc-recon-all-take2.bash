85a0e4f8f1323464fe255b87d31dc3c3503e7878
Paul Wighton <paul.wighton@gmail.com>
2024-09-25 11:25:38 -0400

    [WIP] cox2 script to rerun 4 failed subjects

---
+++ cox2/mlsc-recon-all-take2.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+# `mlsc-recon-all.bash` failed on 4 subjects with a `ERROR: FreeSurfer license file not found`
+# Must have been a network filesystem glitch.  This reruns those 4 subjects
+
+# To submit:
+# sbatch --job-name=cox2_recon --output=cox2_recon_%j.out --mail-type=END,FAIL ./mlsc-recon-all-take2.bash
+
+# To monitor:
+# squeue | grep <username>
+
+#SBATCH --account=fsm
+#SBATCH --partition=basic
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=0-16:00:00
+#SBATCH --output=slurm-%A_%a.out
+
+# PW match the 7 with `wc -l ${SUB_LIST}`
+#SBATCH --array=1-4%4
+
+export SUBJECTS_DIR=/autofs/vast/gerenuk/pwighton/pet/ds004869/fs-subs
+export FREESURFER_HOME=/usr/local/freesurfer/dev
+export MNI152REG_FNIRT=/autofs/vast/gerenuk/pwighton/pet/miba/mni152reg.fnirt
+
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+SUB_LIST=/autofs/vast/gerenuk/pwighton/pet/miba/cox2/recon-all-sub-list-take2.txt
+
+INPUT_FILE=$(cat $SUB_LIST | sed -n "${SLURM_ARRAY_TASK_ID}p" | awk '{print $1}')
+SUB_NAME=$(cat $SUB_LIST | sed -n "${SLURM_ARRAY_TASK_ID}p" | awk '{print $2}')
+
+echo "SUB_NAME:   "${SUB_NAME}
+echo "INPUT_FILE: "${INPUT_FILE}
+
+recon-all -all -i ${INPUT_FILE} -s ${SUB_NAME}
+gtmseg --s ${SUB_NAME}
+mni152reg --s ${SUB_NAME}
+${MNI152REG_FNIRT} --s ${SUB_NAME}
