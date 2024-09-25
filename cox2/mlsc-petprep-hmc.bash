0cd65dd3d39603a3d4705941009acc04b23f3e6f
Paul Wighton <paul.wighton@gmail.com>
2024-09-25 11:36:40 -0400

    [WIP] cox2 mlsc petprep script

---
+++ cox2/mlsc-petprep-hmc.bash
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# To submit:
+# sbatch --job-name=petprep --output=petprep_%j.out --mail-type=END,FAIL ./mlsc-petprep-hmc.bash
+
+# To monitor:
+# squeue | grep <username>
+
+#SBATCH --account=fsm
+#SBATCH --partition=basic
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64G
+#SBATCH --time=0-24:00:00
+#SBATCH --output=slurm-%A_%a.out
+
+INPUT_DIR=/autofs/vast/gerenuk/pwighton/pet/ds004869/ds004869-download
+OUTPUT_DIR=/autofs/vast/gerenuk/pwighton/pet/ds004869/petprep-output
+FS_LICENSE=/autofs/vast/freesurfer/centos7_x86_64/dev/.license
+SINGULARITY_CONTAINER=/autofs/vast/gerenuk/pwighton/pet/petprep_hmc_0.0.8.sif
+# match to `SBATCH --cpus-per-task` above
+NUM_PROCS=32
+
+# Make sure $SINGULARITY_TMPDIR is a local drive 
+# - `/scratch` is local on mlsc
+# - `/scratch/${SLURM_JOBID}` is automatically created when the job starts
+# - see https://it.martinos.org/mlsc-cluster/
+singularity_temp_dir=$(mktemp -d -p /scratch/${SLURM_JOBID} singularity.temp)
+export SINGULARITY_TMPDIR=$singularity_temp_dir
+
+echo "INPUT_DIR:              "${INPUT_DIR}
+echo "OUTPUT_DIR:             "${OUTPUT_DIR}
+echo "FS_LICENSE_FILE:        "${FS_LICENSE}
+echo "SINGULARITY_CONTAINER:  "${SINGULARITY_CONTAINER}
+echo "SINGULARITY_TMPDIR:     "${SINGULARITY_TMPDIR}
+echo "NUM_PROCS:              "${NUM_PROCS}
+
+singularity run \
+    -B ${INPUT_DIR}:/data/input \
+    -B ${OUTPUT_DIR}:/data/output \
+    -B ${FS_LICENSE}:/opt/freesurfer/license.txt \
+    ${SINGULARITY_CONTAINER} \
+      --bids_dir /data/input \
+      --output_dir /data/output \
+      --analysis_level participant \
+      --n_procs ${NUM_PROCS}
